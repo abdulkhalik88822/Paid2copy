11aeff23021d3f1601df230073023906240dc570
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-03-25 21:44:42 +0530

    Add files via upload

---
+++ utils/script.py
@@ -0,0 +1,38 @@
+class script(object):
+    START = """**Hello {} 🤟**
+   
+I am **Your Results Bot**.I am best Channel Link Search Bot! 
+I Will filter your channel posts automatically and send it in your group chat when Someone search it.
+
+Send /help For More"""
+
+    HELP = """To Use me In A Group
+First Buy Subscription With - /buy and Then
+It Will Cost 40₹ Per Month
+
+- Add me in your group & channel with all permissions. 
+- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @CyniteOfficial.
+- After verification send /connect YourChannelID
+- Example : /connect -100xxxxxxxxxx
+- Done ✅. <b><I>Enjoy 💜❤</I></b> )
+
+Remove a Channel with - /disconnect-100xxxxxxxxxxx
+this will help you to remove a indexed channel from your group.
+
+Get connected channels list with - /connections"""
+
+    BUY = """How do you want to pay?"""
+
+    STATS = """My Status 💫
+
+👥 Users: {}
+🧿 Groups: {}"""
+
+    BROADCAST = """<u>{}</u>
+
+Total: `{}`
+Remaining: `{}`
+Success: `{}`
+Failed: `{}`"""
+
+    
