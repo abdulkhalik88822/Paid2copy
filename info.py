642a3e98d40f1b1389a235407073957b8754b990
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-03-25 21:43:36 +0530

    Add files via upload

---
+++ info.py
@@ -0,0 +1,10 @@
+API_ID       = 
+API_HASH     = ""
+BOT_TOKEN    = ""
+SESSION      = ""
+DATABASE_URI = "."
+LOG_CHANNEL  = 
+ADMIN        = 
+CHANNEL      = ""
+APPROVETEXT  = "Hello {mention}\nWelcome To {title}\n\nYour request has been approved"
+APPROVE     = "on"
