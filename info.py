a4da6eb61ae222a7640eef8073bbb66bc016dc5e
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2023-03-27 16:31:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,7 +4,7 @@ BOT_TOKEN    = "6088261601:AAHDYDGB7Cc5Gr6vXZ7VJwvjBKCqzUW_Gw4"
 SESSION      = "BQFFC7IAgDiR4RnLZ71Wp7APite9ZvbxrrqDXtku If7acWMiAxrSYdD7vUbMhQMrndOR_c5c3iY4rqai kcdCvby1d78c-MvORWg5fuFqoCuKyexQcrZWOKJQ pH32Vfb49rmJcK91FIGd9b2JYZDRkQhilQllyDEZ 9hB1ITXacmUA34xUn5EJWtAS5H5HH0Ca07NaxSHO EMxVuvfhlEhfV0tdhXRXp_TO_P-s0x14f1DZXVPK gq5VhnxVrrNKI20j6ZtJUTJKidtkrMtZgzi1TwyN ri-zjp--rTxUeANojpjtvDbk_P39Y3e1nk4FcuiJ bWPq4QAn5kiH1nCi-wUn0n2g6rX-sgAAAAFfe3T4 AA"
 DATABASE_URI = "mongodb+srv://Abdul12:Abdul12@cluster0.o6jx2be.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001896609847
-ADMIN        = 6133440326
+ADMIN        = "6133440326"
 CHANNEL      = "-1001809401952"
 APPROVETEXT  = "Hello {mention}\nWelcome To {title}\n\nYour request has been approved"
 APPROVE     = "on"
