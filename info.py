259ccb00933f23998ea29667b11f73caef3a2be9
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2023-03-28 17:30:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,10 +1,10 @@
-API_ID       = "21723146"
+API_ID       = 21723146
 API_HASH     = "07cd9c82699c28111cb33693ecbd9116"
 BOT_TOKEN    = "6264487502:AAHZMluG6JYyJ_2SVuYDYispJjoM8jeER3o"
 SESSION      = ""
 DATABASE_URI = "mongodb+srv://abdul:abdul@cluster0.tz24m3k.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = "-1001720210775"
-ADMIN        = "6133440326"
+LOG_CHANNEL  = -1001720210775
+ADMIN        = 6133440326
 CHANNEL      = "-1001836465539"
 APPROVETEXT  = "Hello {mention}\nWelcome To {title}\n\nYour request has been approved"
 APPROVE     = "on"
