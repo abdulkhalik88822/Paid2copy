86bbd5bb4a68be90a026bd25d5233a3ce84abfb5
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2023-03-28 09:53:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,10 +1,10 @@
-API_ID       = 6133440326
-API_HASH     = "07cd9c82699c28111cb33693ecbd9116"
-BOT_TOKEN    = "6088261601:AAHDYDGB7Cc5Gr6vXZ7VJwvjBKCqzUW_Gw4"
-SESSION      = "BQFFC7IAgDiR4RnLZ71Wp7APite9ZvbxrrqDXtku If7acWMiAxrSYdD7vUbMhQMrndOR_c5c3iY4rqai kcdCvby1d78c-MvORWg5fuFqoCuKyexQcrZWOKJQ pH32Vfb49rmJcK91FIGd9b2JYZDRkQhilQllyDEZ 9hB1ITXacmUA34xUn5EJWtAS5H5HH0Ca07NaxSHO EMxVuvfhlEhfV0tdhXRXp_TO_P-s0x14f1DZXVPK gq5VhnxVrrNKI20j6ZtJUTJKidtkrMtZgzi1TwyN ri-zjp--rTxUeANojpjtvDbk_P39Y3e1nk4FcuiJ bWPq4QAn5kiH1nCi-wUn0n2g6rX-sgAAAAFfe3T4 AA"
-DATABASE_URI = "mongodb+srv://Abdul12:Abdul12@cluster0.o6jx2be.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1001896609847
-ADMIN        = 6133440326
-CHANNEL      = "-1001809401952"
+API_ID       = 
+API_HASH     = ""
+BOT_TOKEN    = ""
+SESSION      = ""
+DATABASE_URI = ""
+LOG_CHANNEL  = 
+ADMIN        = 
+CHANNEL      = ""
 APPROVETEXT  = "Hello {mention}\nWelcome To {title}\n\nYour request has been approved"
 APPROVE     = "on"
